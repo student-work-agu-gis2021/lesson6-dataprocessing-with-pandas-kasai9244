1c9eefbb07e8cbdd62eeefda30e4c1043e0c936b
kasai9244 <>
2021-05-21 03:01:29 +0000

    I added the code,"na_values[-9999]".

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -18,8 +18,7 @@ data = None
 
 # YOUR CODE HERE 1
 fp='data/1091402.txt'
-data = pd.read_csv(fp, skiprows=2,delim_whitespace=True)
-data.fillna(-9999)
+data = pd.read_csv(fp, skiprows=2,delim_whitespace=True,na_values=[-9999])
 
 
 # ### Part 2 
